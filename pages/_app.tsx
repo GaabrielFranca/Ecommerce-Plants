2c41e89b5b13fd16615ce6aaa9bbedf4932a7c70
Gabriel França <gabrielfran1864@gmail.com>
2022-12-24 10:52:36 -0300

    add Progress bar on routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,31 @@ import { ContextStorage } from "../context/useContext";
 import type { AppProps } from "next/app";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import ProgressBar from "@badrap/bar-of-progress";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+const progress = new ProgressBar({
+  size: 4,
+  color: "#898C5E",
+  className: "bar-of-progress",
+  delay: 100,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", progress.start);
+    router.events.on("routeChangeComplete", progress.finish);
+    router.events.on("routeChangeError", progress.finish);
+
+    return () => {
+      router.events.off("routeChangeStart", progress.start);
+      router.events.off("routeChangeComplete", progress.finish);
+      router.events.off("routeChangeError", progress.finish);
+    };
+  }, [router]);
+
   return (
     <>
       <ToastContainer position="top-center" />
