0941502d7ce72c2420ce671409272a3d510a6aa7
Gabriel França <gabrielfran1864@gmail.com>
2022-12-26 19:04:24 -0300

    Create Success page.

---
+++ pages/success/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="h-[80vh] flex justify-center items-center">
+      <div className="">
+        <h1 className="font-title text-lg sm:text-2xl md::text-3xl lg:text-4xl">
+          Pagamento concluido com sucesso
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default index;
